5bfe88c9ff3609fad798e859fedad59f8ad5c870
Rico Rueedi <metabomatching@unil.ch>
2018-02-14 15:03:39 +0100

    New test data, new test function.
    Former-commit-id: bc8b636355225c1b883eaa6d3a9f01467dd13d99

--- ts_isa.m
+++ ts_isa.m
@@ -1,10 +1,28 @@
+function ts = ts_isa(ix,addNaNs)
+if nargin<2; addNaNs = 0; end
+if nargin<1; ix = 0 ; end
+
+lof = dir('ts/ts.*.csv');
+if ix == 0 | ix > length(lof)
+  fprintf('\nrun this test as a function of the input file index, chosing among the following: \n\n')
+  for ii = 1:length(lof)
+    fprintf('%2d -- %s\n',ii,lof(ii).name);
+  end
+  fprintf('\nA file named ts.ab.cd.csv contains a matrix with b*10^a rows and d*10^c columns.\n');
+  return
+end
 %
 % ----- ----------------
 %       import test data
 %       ---------------- -----
 %
-ts.mx = csvread( 'ts/ts.csv' );
-ts.mx(ts.mx==0) = NaN;
+lof = dir('ts/ts.*.csv');
+ts.mx = csvread(fullfile('ts',lof(ix).name));
+if addNaNs
+  dd = prod(size(ts.mx));
+  se = randi(dd,ceil(dd/100),1);
+  ts.mx(se) = NaN;
+end
 %
 % ----- ------------------
 %       set ISA parameters
@@ -20,6 +38,7 @@ ts.isa.sigc = 1;
 %       run ISA 
 %       ------- -----
 %
+tic;
 [ts.isa.cs_scf,ts.isa.gs_sgf,ts.isa.ns_tf,ts.isa.info,ts.isa.s_rf,ts.isa.r_floors] = ...
     isa_base(ts.mx ...
     ,'DeltaSame',  ts.isa.delta_same ...
@@ -28,3 +47,4 @@ ts.isa.sigc = 1;
     ,'SignatureG', ts.isa.sigg ...
     ,'SignatureC', ts.isa.sigc ...
     );
+ts.runtime=toc;
