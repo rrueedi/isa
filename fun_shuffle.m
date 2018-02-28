78193cd04134cb40e314e65e3144f78fc6f738f7
Rico Rueedi <metabomatching@unil.ch>
2018-02-28 18:08:14 +0100

    New shuffle function. Three times faster in octave, about the same in Matlab.

--- fun_shuffle.m
+++ fun_shuffle.m
@@ -1,6 +1,3 @@
 function cg_ZZ = fun_shuffle(cg_EE)
-
-[nc ng] = size(cg_EE);
-[dummy,permutation] = sort(rand(nc*ng,1));
-
-cg_ZZ = reshape(cg_EE(permutation),nc,ng);
+x = size(cg_EE);
+cg_ZZ = reshape(cg_EE(randperm(prod(x))),x);
